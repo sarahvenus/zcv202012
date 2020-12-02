13839749ccee064007f57729e2bed824eed55f43
sarahvenus <73270035+sarahvenus@users.noreply.github.com>
2020-12-01 23:19:00 -0800

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(
 )
 
 app.get('/', (request, response) => {
-  response.json({ info: 'Node.js, Express, and Postgres API' })
+  response.json({ info: 'Node.js, Express, and Postgres APIw' })
 })
 
 app.get('/users', db.getUsers)
