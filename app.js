f16e3eaa8317d8d3c84d574894d743b957ea0cc5
sarahvenus <cavizah@gmail.com>
2020-12-02 20:07:34 -0800

    Ver2

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(
 )
 
 app.get('/', (request, response) => {
-  response.json({ info: 'Node.js, Express, and Postgres APIw' })
+  response.json({ info: 'ZC04556' })
 })
 
 app.get('/users', db.getUsers)
