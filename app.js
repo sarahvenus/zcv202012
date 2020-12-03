9f3dacb54c0ab43543d389208b04f143f7f9d530
sarahvenus <73270035+sarahvenus@users.noreply.github.com>
2020-12-02 18:03:22 -0800

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(
 )
 
 app.get('/', (request, response) => {
-  response.json({ info: 'ZCV CRUD 20201202' })
+  response.json({ info: 'ZCV CRUD 20201202 vf' })
 })
 
 app.get('/users', db.getUsers)
